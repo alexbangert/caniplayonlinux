0790081ba6780e1ea10b8f87297817f3679e607b
Alex Bangert <alex@alexbangert.dev>
2024-10-03 14:37:08 +0200

    load sorted list

--- src/components/molecules/GameList.tsx
+++ src/components/molecules/GameList.tsx
@@ -1,12 +1,18 @@
 import { GameCard } from "./GameCard";
 
 export default async function GameList({}) {
-  const games = [289070];
+  const games = [
+    { id: 1280930, hours: 3 },
+    { id: 1091500, hours: 11 },
+    { id: 2429640, hours: 6 },
+    { id: 289070, hours: 66 },
+    { id: 1085660, hours: 22 },
+  ].sort((a, b) => b.hours - a.hours);
 
   return (
     <>
       {games.map((g) => (
-        <GameCard appId={g} key={g} />
+        <GameCard appId={g.id} key={g.id} />
       ))}
     </>
   );
